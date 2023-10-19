3b0d8fd0aeb147ef43dc48b7233866d52cdaa62a
SergeyFM <github@svistunov.su>
2023-10-19 10:47:25 +0300

    New Cashier Does Not Know About Space or Shift

---
+++ New_Cashier_Does_Not_Know_About_Space_or_Shift.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingChallenges;
+[TestClass]
+public class New_Cashier_Does_Not_Know_About_Space_or_Shift {
+    [TestMethod]
+    public void Test() {
+
+        /*
+
+        New Cashier Does Not Know About Space or Shift
+
+        Some new cashiers started to work at your restaurant.
+        They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+        All the orders they create look something like this:
+        "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+        The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+        Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+        "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+        The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+        The menu items are fairly simple, there is no overlap in the names of the items:
+        1. Burger
+        2. Fries
+        3. Chicken
+        4. Pizza
+        5. Sandwich
+        6. Onionrings
+        7. Milkshake
+        8. Coke
+
+        */
+
+        Assert.AreEqual("Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke", GetOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"));
+    }
+
+    public static string GetOrder(string input) {
+        string[] items = "Burger Fries Chicken Pizza Sandwich Onionrings Milkshake Coke".Split(" ");
+        foreach (string item in items) input = input.Replace(item.ToLower(), " " + item + " ");
+        return string.Join(" ", input.Trim().Split(" ").Where(i=> !string.IsNullOrWhiteSpace(i)).OrderBy(i => Array.IndexOf(items, i)));
+    }
+}
